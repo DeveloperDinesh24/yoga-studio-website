2e31f010cd77e9a5475f3c5c07ea9ebf33e3402f
Dinesh <1442dinesh6singh5@gmail.com>
2026-01-25 18:02:38 +0530

    feat: make header little less opacity

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import '../styles/Header.css'
 
 export default function Header() {
   return (
-    <header className='max-w-600 mx-auto w-full h-14 bg-[#403F2B] fixed z-50 top-0 flex items-center justify-between px-8 text-[#F3F1C4]'>
+    <header className='max-w-600 mx-auto w-full h-14 bg-[#403F2B88] fixed z-50 top-0 flex items-center justify-between px-8 text-[#F3F1C4]'>
       <span className='text-2xl'>Yoga Studio</span>
       <nav className='hidden md:block'>
         <ul className='flex gap-6 text-xl'>

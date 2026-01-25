a189ca387012b1f5e47e8e5780b87aac13c63de1
Dinesh <1442dinesh6singh5@gmail.com>
2026-01-25 18:37:50 +0530

    feat: complete programs section

--- src/components/ProgramsSection.tsx
+++ src/components/ProgramsSection.tsx
@@ -1,7 +1,129 @@
 export default function ProgramsSection() {
   return (
-    <section id="programs">
+    <section id='programs' className='w-full min-h-dvh max-w-600 mx-auto p-14'>
+      <h2 className='text-6xl text-center'>Our Programs</h2>
 
+      <div className='w-full h-full py-24 flex flex-col'>
+        <div className='max-w-400 mx-auto flex gap-28 justify-between items-center'>
+          <div className='w-120'>
+            <img
+              className=''
+              src='../src/assets/program-image.png'
+              alt='Program Image'
+            />
+          </div>
+          <div className='w-120 space-y-6'>
+            <h3 className='text-[26px]'>
+              Program Name:{' '}
+              <span className='text-3xl'>Lorem, ipsum dolor.</span>
+            </h3>
+            <p className='text-xl'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <p className='text-xl'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta
+              modi rem esse tenetur inventore eveniet rerum, aliquid laboriosam
+              repellendus sapiente! Recusandae, quasi modi?
+            </p>
+            <p className='text-xl'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta
+              modi rem esse tenetur inventore eveniet rerum, aliquid laboriosam
+              repellendus sapiente! Recusandae, quasi modi?
+            </p>
+          </div>
+        </div>
+        <hr className='max-w-300 w-full mx-auto my-8' />
+        <div className='max-w-400 mx-auto flex flex-row-reverse gap-28 justify-between items-center'>
+          <div className='w-120'>
+            <img
+              className=''
+              src='../src/assets/program-image.png'
+              alt='Program Image'
+            />
+          </div>
+          <div className='w-120 space-y-6'>
+            <h3 className='text-[26px]'>
+              Program Name:{' '}
+              <span className='text-3xl'>Lorem, ipsum dolor.</span>
+            </h3>
+            <p className='text-xl'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <p className='text-xl'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta
+              modi rem esse tenetur inventore eveniet rerum, aliquid laboriosam
+              repellendus sapiente! Recusandae, quasi modi?
+            </p>
+            <p className='text-xl'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta
+              modi rem esse tenetur inventore eveniet rerum, aliquid laboriosam
+              repellendus sapiente! Recusandae, quasi modi?
+            </p>
+          </div>
+        </div>
+        <hr className='max-w-300 w-full mx-auto my-8' />
+        <div className='max-w-400 mx-auto flex gap-28 justify-between items-center'>
+          <div className='w-120'>
+            <img
+              className=''
+              src='../src/assets/program-image.png'
+              alt='Program Image'
+            />
+          </div>
+          <div className='w-120 space-y-6'>
+            <h3 className='text-[26px]'>
+              Program Name:{' '}
+              <span className='text-3xl'>Lorem, ipsum dolor.</span>
+            </h3>
+            <p className='text-xl'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <p className='text-xl'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta
+              modi rem esse tenetur inventore eveniet rerum, aliquid laboriosam
+              repellendus sapiente! Recusandae, quasi modi?
+            </p>
+            <p className='text-xl'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta
+              modi rem esse tenetur inventore eveniet rerum, aliquid laboriosam
+              repellendus sapiente! Recusandae, quasi modi?
+            </p>
+          </div>
+        </div>
+        <hr className='max-w-300 w-full mx-auto my-8' />
+        <div className='max-w-400 mx-auto flex flex-row-reverse gap-28 justify-between items-center'>
+          <div className='w-120'>
+            <img
+              className=''
+              src='../src/assets/program-image.png'
+              alt='Program Image'
+            />
+          </div>
+          <div className='w-120 space-y-6'>
+            <h3 className='text-[26px]'>
+              Program Name:{' '}
+              <span className='text-3xl'>Lorem, ipsum dolor.</span>
+            </h3>
+            <p className='text-xl'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <p className='text-xl'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta
+              modi rem esse tenetur inventore eveniet rerum, aliquid laboriosam
+              repellendus sapiente! Recusandae, quasi modi?
+            </p>
+            <p className='text-xl'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta
+              modi rem esse tenetur inventore eveniet rerum, aliquid laboriosam
+              repellendus sapiente! Recusandae, quasi modi?
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }

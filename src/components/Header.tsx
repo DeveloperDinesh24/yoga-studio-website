e1361613988098783b16cd3248cc42a90a4cf804
Dinesh <1442dinesh6singh5@gmail.com>
2026-01-25 14:30:58 +0530

    feat: complete hero section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import '../styles/Header.css'
 
 export default function Header() {
   return (
-    <header className='w-full h-14 bg-[#403F2B] fixed top-0 flex items-center justify-between px-8 text-[#F3F1C4]'>
+    <header className='w-full h-14 bg-[#403F2B] fixed z-50 top-0 flex items-center justify-between px-8 text-[#F3F1C4]'>
       <span className='text-2xl'>Yoga Studio</span>
       <nav className='hidden md:block'>
         <ul className='flex gap-6 text-xl'>
@@ -10,13 +10,13 @@ export default function Header() {
             <li className='cursor-pointer section-links'>Home</li>
           </a>
           <a href='#about'>
-            <li className=' cursor-pointer section-links'>Classes</li>
+            <li className='section-links'>Classes</li>
           </a>
           <a href='#about'>
-            <li className='cursor-pointer section-links'>About</li>
+            <li className='section-links'>About</li>
           </a>
           <a href='#contact'>
-            <li className='cursor-pointer section-links'>Contact</li>
+            <li className='section-links'>Contact</li>
           </a>
         </ul>
       </nav>
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,7 +2,7 @@ export default function HeroSection() {
   return (
     <section
       id='hero'
-      className='w-full h-[85dvh] bg-[url(../assets/hero-section-background.jpg)] bg-contain bg-no-repeat text-[#F3F1C4] relative'
+      className='w-full h-[85dvh] bg-[url(../assets/hero-section-background.jpg)] bg-cover bg-center bg-no-repeat text-[#F3F1C4] relative'
     >
       <div className='w-full h-full bg-[#00000055] flex flex-col items-center justify-center'>
         <h1 className='text-8xl font-semibold text-center mb-2'>
@@ -12,9 +12,9 @@ export default function HeroSection() {
         <button className='w-fit h-12 text-lg px-12 rounded-4xl outline outline-[#F3F1C4] cursor-pointer hover:bg-[#F3F1C444] '>
           Join Us
         </button>
-        <div className="w-5 rounded-6xl outline">
-          <i class="fa-solid fa-arrow-down"></i>
-        </div>
+        <a href="#" className='absolute bottom-4 text-2xl text-black'>
+          <i className="fa-regular fa-circle-down"></i>
+        </a>
       </div>
     </section>
   )

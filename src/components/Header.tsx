c0f17af4cebc2778dc3780eba76bc3b8e710e024
Dinesh <1442dinesh6singh5@gmail.com>
2026-01-25 13:40:05 +0530

    feat: complete header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,26 @@
+import '../styles/Header.css'
+
 export default function Header() {
   return (
-    <header className="w-full h-14 bg-[#403F2B] fixed top-0 flex items-center justify-between px-4 text-[#F3F1C4]">
-      <span className="text-2xl">
-        Yoga Studio
-      </span>
-      <nav>
-        <ul className="flex gap-4 text-xl">
-          <a href="#hero"><li className="hover:text-white cursor-pointer">Home</li></a>
-          <a href="#about"><li className="hover:text-white cursor-pointer">Classes</li></a>
-          <a href="#about"><li className="hover:text-white cursor-pointer">About</li></a>
-          <a href="#contact"><li className="hover:text-white cursor-pointer">Contact</li></a>
+    <header className='w-full h-14 bg-[#403F2B] fixed top-0 flex items-center justify-between px-8 text-[#F3F1C4]'>
+      <span className='text-2xl'>Yoga Studio</span>
+      <nav className='hidden md:block'>
+        <ul className='flex gap-6 text-xl'>
+          <a href='#hero'>
+            <li className='cursor-pointer section-links'>Home</li>
+          </a>
+          <a href='#about'>
+            <li className=' cursor-pointer section-links'>Classes</li>
+          </a>
+          <a href='#about'>
+            <li className='cursor-pointer section-links'>About</li>
+          </a>
+          <a href='#contact'>
+            <li className='cursor-pointer section-links'>Contact</li>
+          </a>
         </ul>
       </nav>
+      <div className='text-2xl block md:hidden cursor-pointer'>☰</div>
     </header>
   )
 }

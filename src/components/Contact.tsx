38b7d65199ccbddf09fd1ca7e60d8106866acc88
Dinesh <1442dinesh6singh5@gmail.com>
2026-02-03 21:36:30 +0530

    fix: add media breakpoints for contact section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,7 +2,7 @@ export default function Contact() {
   return (
     <section
       id='contact'
-      className='w-full bg-[#1a2e1a] max-w-full mx-auto py-20 min-h-dvh text-stone-100 font-sans'
+      className='w-full bg-[#1a2e1a] max-w-full mx-auto py-20 px-8 min-h-dvh text-stone-100 font-sans'
     >
       <div className='max-w-6xl mx-auto px-6'>
         <div className='text-center mb-16'>
@@ -14,7 +14,7 @@ export default function Contact() {
           </p>
         </div>
 
-        <div className='flex flex-col lg:flex-row gap-12 items-stretch'>
+        <div className='flex flex-col lg:flex-row gap-12 items-center'>
           <div className='w-full lg:w-1/2 bg-stone-50 p-8 md:p-12 rounded-2xl shadow-2xl'>
             <h3 className='text-2xl text-stone-800 font-semibold mb-8'>
               Send a Message
@@ -51,7 +51,7 @@ export default function Contact() {
               </button>
             </form>
           </div>
-          <div className='w-full lg:w-1/2 min-h-100 rounded-2xl overflow-hidden shadow-2xl border-4 border-stone-800/20'>
+          <div className='w-full lg:w-1/2 max-h-100 h-100 rounded-2xl overflow-hidden shadow-2xl border-4 border-stone-800/20 lg:scale-109'>
             <iframe
               src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.610357219642!2d74.62523442451787!3d26.51759089241468!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396be76f74e59e81%3A0x5ef95f883f62c064!2sShivoham%20Yog%20By%20Kanan!5e1!3m2!1sen!2sin!4v1770123224664!5m2!1sen!2sin'
               className='w-full h-full grayscale-20 contrast-[1.1]'

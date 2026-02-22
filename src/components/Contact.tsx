0746c615afca77dde5ec0f20fde9f0d5d9c3ca5c
Dinesh <1442dinesh6singh5@gmail.com>
2026-02-22 12:23:00 +0530

    feat: change about us image and change map location

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -53,7 +53,7 @@ export default function Contact() {
           </div>
           <div className='w-full lg:w-1/2 max-h-100 h-100 rounded-2xl overflow-hidden shadow-2xl border-4 border-stone-800/20 lg:scale-109'>
             <iframe
-              src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.610357219642!2d74.62523442451787!3d26.51759089241468!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396be76f74e59e81%3A0x5ef95f883f62c064!2sShivoham%20Yog%20By%20Kanan!5e1!3m2!1sen!2sin!4v1770123224664!5m2!1sen!2sin'
+              src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4627.000231484026!2d74.6311183879907!3d26.4752837192307!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396be7aa46a24ee1%3A0xd6ba63c3af8fdcad!2s123%2C%20Ana%20Sagar%20Link%20Rd%2C%20Mali%20Mohalla%2C%20Ajmer%2C%20Rajasthan%20305001!5e1!3m2!1sen!2sin!4v1771743068529!5m2!1sen!2sin'
               className='w-full h-full grayscale-20 contrast-[1.1]'
               loading='lazy'
               referrerPolicy='no-referrer-when-downgrade'
